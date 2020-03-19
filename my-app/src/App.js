0463a9f511c66b579f48b5cdd77d6d9b4ae40b74
Léa Youssef <leaysf@gmail.com>
2020-03-19 18:27:59 +0100

    update_4 more infos

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,33 +4,14 @@ import './App.css';
 import Data from './result.json';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ImageBackground from 'react-bootstrap/Image'
-/*import {BrowserRouter as Router} from "react-router-dom";
-import {
-  MDBNavbar,
-  MDBNavbarBrand,
-  MDBNavbarNav,
-  MDBNavItem,
-  MDBNavLink,
-  MDBNavbarToggler,
-  MDBCollapse,
-  MDBMask,
-  MDBRow,
-  MDBCol,
-  MDBBtn,
-  MDBView,
-  MDBContainer,
-  MDBFormInline,
-  MDBAnimation
-} from "mdbreact";*/
 import restaurant_img from './restaurant-3.jpg'
 
 function App() {
   return (
     <html>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
-    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <div className="App">
     <ImageBackground src={restaurant_img} fluid/>
       <header className="App-header">
@@ -56,28 +37,33 @@ function App() {
       </header>
       <body className="App-maincontent">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          
+        </p>
         <div class="App-maincontent">
-          <div>
-            <br />
-            <p>
-
-            </p>
-            <ul className="liste_resto">          
+          <script src='./filtering.js'/>
+          <input class="form-control" id="my_input" type="text" placeholder="Search.." onKeuUp="myFunction()"/>
+          <p></p>
+            <table class="table table-dark" id="my_table"> 
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Adress</th>
+                <th>Price</th>
+                <th>Experience</th>
+              </tr>
+            </thead>         
                   {Data.map((resto,index) => {
-                  return <li align="left">
-                    <p align="left" className="title">
-                    {resto.name}
-                    </p>
-                    <p align="left">
-                    {resto.phone}
-                    </p>
-                    <p>
-                    {resto.address}
-                    </p>
-                  </li>
+                  return <tr>
+                    <td><a className="App-link" href={resto.url} target="_blank" rel="noopener noreferrer">{resto.name}</a></td>
+                    <td>{resto.phone}</td>
+                    <td>{resto.address}</td>
+                    <td>{resto.price}</td>
+                    <td>{resto.experience}</td>
+                  </tr>
                 })}
-            </ul>
-          </div>
+            </table>
         </div>
       </body>
       <footer>
@@ -89,7 +75,6 @@ function App() {
     </html>
   );
 }
-
 
 
 export default App;
